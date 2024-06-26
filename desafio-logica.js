ea6ad65c4ad696f4a1df1fd15d60a09e81f6dfb9
Renan <j.renan.r@gmail.com>
2024-06-26 17:03:57 -0300

    comecando a desenvolver a logica

---
+++ desafio-logica.js
@@ -0,0 +1,31 @@
+let heroi = 'arthur'
+let xp = 1001;
+
+let classe = "";
+if (xp <= 1000) {
+    classe = "Ferro"
+
+
+} else if (xp <= 2000) {
+    console.log("Hoje é segunda.")
+
+}
+else if (xp <= 5000) {
+    console.log("Hoje é terça.")
+
+} else if (xp <= 7000) {
+    console.log("Hoje é quarta.")
+
+} else if (xp <= 8000) {
+    console.log("Hoje é quinta.")
+
+} else if (xp <= 9000) {
+    console.log("Hoje é sexta.")
+
+} else {
+    
+
+}
+
+
+console.log(`O Herói de nome ${heroi} está no nível de ${classe}`)
